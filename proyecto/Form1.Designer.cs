8d4ff77a8b41ed378f7cd5b88923ea1b97c45378
guasa <guasa@JEINADWIN19>
2026-01-15 08:26:24 -0500

    prueba

--- proyecto/Form1.Designer.cs
+++ proyecto/Form1.Designer.cs
@@ -28,42 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
-			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-			this.btnInicio = new System.Windows.Forms.Button();
-			this.SuspendLayout();
-			// 
-			// btnInicio
-			// 
-			this.btnInicio.BackgroundImage = global::proyecto.Properties.Resources.button_for_starting_game_clicking_play_vector;
-			this.btnInicio.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-			this.btnInicio.Cursor = System.Windows.Forms.Cursors.Hand;
-			this.btnInicio.Location = new System.Drawing.Point(351, 319);
-			this.btnInicio.Name = "btnInicio";
-			this.btnInicio.Size = new System.Drawing.Size(99, 46);
-			this.btnInicio.TabIndex = 0;
-			this.btnInicio.UseVisualStyleBackColor = true;
-			this.btnInicio.Click += new System.EventHandler(this.btnInicio_Click);
-			// 
-			// Form1
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.BackgroundImage = global::proyecto.Properties.Resources._1;
-			this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Controls.Add(this.btnInicio);
-			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-			this.Name = "Form1";
-			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-			this.Text = "Form1";
-			this.Load += new System.EventHandler(this.Form1_Load);
-			this.ResumeLayout(false);
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnInicio1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnInicio1
+            // 
+            this.btnInicio1.BackgroundImage = global::proyecto.Properties.Resources.button_for_starting_game_clicking_play_vector;
+            this.btnInicio1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnInicio1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnInicio1.Location = new System.Drawing.Point(234, 207);
+            this.btnInicio1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnInicio1.Name = "btnInicio1";
+            this.btnInicio1.Size = new System.Drawing.Size(66, 30);
+            this.btnInicio1.TabIndex = 0;
+            this.btnInicio1.UseVisualStyleBackColor = true;
+            this.btnInicio1.Click += new System.EventHandler(this.btnInicio_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::proyecto.Properties.Resources._1;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(533, 292);
+            this.Controls.Add(this.btnInicio1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button btnInicio;
+        private System.Windows.Forms.Button btnInicio1;
     }
 }
 
